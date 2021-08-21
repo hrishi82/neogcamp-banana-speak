9513902c10fb5a98552169080af29c1278d366a4
Barman Hrishikesh <hrishikesh.barman@volvo.com>
2021-08-21 18:56:26 +0530

    Input output processing added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello!");
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector('#input-text')
+var outputBox = document.querySelector('#output-box')
+
+
+
+function clickFunc(){
+    outputBox.innerText = "aaaaddeqfwefwf " + inputText.value;
+    
+}
+btnTranslate.addEventListener("click", clickFunc);
