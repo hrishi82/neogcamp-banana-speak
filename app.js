1bd7effeb5856e7a07413bcccfad131340600013
Barman Hrishikesh <hrishikesh.barman@volvo.com>
2021-08-23 03:00:43 +0530

    updated api

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector('#input-text')
 var outputBox = document.querySelector('#output-box')
 
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var urlConverter = (text) => {
+    return url + "?text=" + text;
+};
 
 
-function clickFunc(){
-    outputBox.innerText = "aaaaddeqfwefwf " + inputText.value;
-    
+function errorHandler(error){
+    console.log(error);
 }
-btnTranslate.addEventListener("click", clickFunc);
+
+var dataFetch = () => {
+    var inptxt =  inputText.value
+    fetch(urlConverter(inptxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputBox.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+
+    
+btnTranslate.addEventListener("click", dataFetch);
